a68469725cfe8bd327696e3d15c32092fa54318e
Paul Butcher <paul@paulbutcher.com>
2026-08-15 21:17:52 +0000

    Add copyright headers

--- Todo.lean
+++ Todo.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Paul Butcher. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+
 import Todo.Db
 import Todo.Links
 import Todo.Views
--- Todo/App.lean
+++ Todo/App.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Paul Butcher. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+
 import Std.Http.Server
 import SQLite
 import Html
